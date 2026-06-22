682e65721d01da54a62dead6625cc9ae077170b3
duranyarith906faria-spec <duranyarith.9.06faria@gmail.com>
2026-06-22 03:16:15 -0500

    Fix: CORS y paquete

---
+++ src/main/java/com/traveling/Configuraciones/WebConfig.java
@@ -0,0 +1,17 @@
+package  com.traveling.Configuraciones;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") 
+                .allowedOrigins("https://duranyarith906faria-spec-traveling.vercel.app") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
